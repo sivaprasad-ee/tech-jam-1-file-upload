9ffeb4af3df2b588e2d1eb4a501c34ac43e684f3
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2022-03-30 07:54:58 +0530

    Adding FileUploads using Streaming approach code

--- spring-boot-file-upload/src/main/java/com/everest/fileupload/FileUploadController.java
+++ spring-boot-file-upload/src/main/java/com/everest/fileupload/FileUploadController.java
@@ -1,15 +1,17 @@
 package com.everest.fileupload;
 
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.util.Streams;
 import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 
 @Controller
@@ -22,29 +24,26 @@ public class FileUploadController {
         return "index";
     }
 
-    @PostMapping("/single-upload")
-    public String handleSingleFileUpload(@RequestParam("singleFile") MultipartFile multipartFile,
-                                         RedirectAttributes redirectAttributes) throws IOException {
-        InputStream inputStream = multipartFile.getInputStream();
-        String originalFilename = multipartFile.getOriginalFilename();
-        File targetFile = new File(uploadsDir, originalFilename);
-        FileUtils.copyInputStreamToFile(inputStream, targetFile);
-        redirectAttributes.addFlashAttribute("msg", "Single file uploaded successfully");
+    @PostMapping("/streaming-upload")
+    public String handleStreamingFileUpload(HttpServletRequest request,
+                                                  RedirectAttributes redirectAttributes) throws Exception {
+        ServletFileUpload upload = new ServletFileUpload();
+        FileItemIterator iterStream = upload.getItemIterator(request);
+        while (iterStream.hasNext()) {
+            FileItemStream item = iterStream.next();
+            String name = item.getName();
+            InputStream stream = item.openStream();
+            if (name != null && !name.isBlank() && !item.isFormField()) {
+                // Process the InputStream
+                File targetFile = new File(uploadsDir, name);
+                FileUtils.copyInputStreamToFile(stream, targetFile);
+            } else {
+                String formFieldValue = Streams.asString(stream);
+                System.out.println("Field Name: "+ item.getFieldName() + ", value: "+ formFieldValue);
+            }
+        }
+
+        redirectAttributes.addFlashAttribute("msg", "File(s) uploaded successfully");
         return "redirect:/";
     }
-
-    @PostMapping("/multiple-upload")
-    public String handleMultipleFilesUpload(@RequestParam("file1") MultipartFile multipartFile1,
-                                            @RequestParam("file2") MultipartFile multipartFile2,
-                                         RedirectAttributes redirectAttributes) throws IOException {
-        File targetFile1 = new File(uploadsDir, multipartFile1.getOriginalFilename());
-        FileUtils.copyInputStreamToFile(multipartFile1.getInputStream(), targetFile1);
-
-        File targetFile2 = new File(uploadsDir, multipartFile2.getOriginalFilename());
-        FileUtils.copyInputStreamToFile(multipartFile2.getInputStream(), targetFile2);
-
-        redirectAttributes.addFlashAttribute("msg", "Multiple files uploaded successfully");
-        return "redirect:/";
-    }
-
 }
