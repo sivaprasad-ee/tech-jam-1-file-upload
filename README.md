2c7bc1afdafd3d2b4d4fc8691d1aa7bebaef3953
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2022-03-31 08:22:27 +0530

    Adding FileUploads using Chunked approach code

--- README.md
+++ README.md
@@ -1,16 +1,49 @@
 # TechJam - File Upload
 
+## Agenda:
+* Learn more about how file upload functionality works?
+* How to deal with large file uploads?
+* How to handle file uploads in efficient manner?
+
+## Stages
+
+### Basic File Uploading
 1. Simple file upload
+2. Multiple files upload.
+3. File uploading using Streaming approach.
+4. Break and do parallel upload chunks
+
+### Advanced File Uploading
+1. Upload optimizations - can images be skewed, videos reduced size
 2. Region based uploads CDN friendly?
-3. 10 files batch upload.
-4. Break and do parallel upload chunks (similar to Hadoop)
-5. Upload optimizations - can images be skewed, videos reduced size
-6. How does zoom save video on cloud vs Desktop.
-7. P2P uploads similar to torrents.
-8. How does google drive, dropbox or iCloud upload files?
-9. How deltas are managed in cloud shared drives?
+3. P2P uploads similar to torrents.
+4. How does zoom save video on cloud vs Desktop.
+5. How does google drive, dropbox or iCloud upload files?
+6. How deltas are managed in cloud shared drives?
 
 
 ## Sample Large File Sources
 * http://xcal1.vodafone.co.uk/
 * https://fastest.fish/test-files
+
+## How HTTP FileUpload Works?
+
+```shell
+POST /upload HTTP/1.1
+Host: localhost:3000
+Content-Length: 1325
+Origin: http://localhost:3000
+... other headers ...
+Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryePkpFF7tjBAqx29L
+
+------WebKitFormBoundaryePkpFF7tjBAqx29L
+Content-Disposition: form-data; name="age"
+
+25
+------WebKitFormBoundaryePkpFF7tjBAqx29L
+Content-Disposition: form-data; name="file"; filename="upload.png"
+Content-Type: image/png
+
+... contents of file goes here ...
+------WebKitFormBoundaryePkpFF7tjBAqx29L--
+```
--- spring-boot-file-upload/src/main/java/com/everest/fileupload/FileUploadController.java
+++ spring-boot-file-upload/src/main/java/com/everest/fileupload/FileUploadController.java
@@ -1,50 +1,51 @@
 package com.everest.fileupload;
 
-import org.apache.commons.io.FileUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Map;
+import java.util.UUID;
 
 @Controller
+@Slf4j
 public class FileUploadController {
 
     private static final String uploadsDir = "storage";
 
     @GetMapping
     public String index() {
-        return "index";
+        return "chunked";
     }
 
     @PostMapping("/single-upload")
-    public String handleSingleFileUpload(@RequestParam("singleFile") MultipartFile multipartFile,
-                                         RedirectAttributes redirectAttributes) throws IOException {
-        InputStream inputStream = multipartFile.getInputStream();
-        String originalFilename = multipartFile.getOriginalFilename();
+    @ResponseBody
+    public Map<String, String> handleSingleFileUpload(
+            @RequestParam(value = "videoId", required = false) String videoId,
+            @RequestParam("file") MultipartFile multipartFile) throws IOException {
+        if(videoId==null || videoId.isBlank()) {
+            videoId = UUID.randomUUID().toString();
+        }
+        String originalFilename = videoId + "-" + multipartFile.getOriginalFilename();
         File targetFile = new File(uploadsDir, originalFilename);
-        FileUtils.copyInputStreamToFile(inputStream, targetFile);
-        redirectAttributes.addFlashAttribute("msg", "Single file uploaded successfully");
-        return "redirect:/";
-    }
+        if(!targetFile.exists()){
+            targetFile.createNewFile();
+        }
+        Files.write(
+                Paths.get(targetFile.getAbsolutePath()),
+                multipartFile.getBytes(),
+                StandardOpenOption.APPEND);
 
-    @PostMapping("/multiple-upload")
-    public String handleMultipleFilesUpload(@RequestParam("file1") MultipartFile multipartFile1,
-                                            @RequestParam("file2") MultipartFile multipartFile2,
-                                         RedirectAttributes redirectAttributes) throws IOException {
-        File targetFile1 = new File(uploadsDir, multipartFile1.getOriginalFilename());
-        FileUtils.copyInputStreamToFile(multipartFile1.getInputStream(), targetFile1);
-
-        File targetFile2 = new File(uploadsDir, multipartFile2.getOriginalFilename());
-        FileUtils.copyInputStreamToFile(multipartFile2.getInputStream(), targetFile2);
-
-        redirectAttributes.addFlashAttribute("msg", "Multiple files uploaded successfully");
-        return "redirect:/";
+        return Map.of("videoId", videoId);
     }
 
 }
