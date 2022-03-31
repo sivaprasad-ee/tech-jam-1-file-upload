06569e558cabdc720950f6135f55f5c82bb1d9bd
Ravi Bolla <ravi.bolla@everest.engineering>
2022-03-31 14:47:36 +0530

    Add client page for upload

--- node-file-upload/server.js
+++ node-file-upload/server.js
@@ -6,23 +6,14 @@ const config = require('config');
 const filePath = config.get('server.upload_file_path');
 
 http.createServer(function (req, res) {
-    if (req.url == '/upload') {
-        const form = new formidable.IncomingForm();
-        form.parse(req, function (err, fields, files) {
-            var filepath = files.filetoupload.filepath;
-            var newpath = `${filePath}/${files.filetoupload.originalFilename}`;
-            fs.rename(filepath, newpath, function (err) {
-                if (err) throw err;
-                res.write('File uploaded!');
-                res.end();
-            });
+    const form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, data) {
+        var filepath = data.filetoupload.filepath;
+        var newpath = `${filePath}/${data.filetoupload.originalFilename}`;
+        fs.rename(filepath, newpath, function (err) {
+            if (err) throw err;
+            res.write('File uploaded!');
+            res.end();
         });
-    } else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<form action="upload" method="post" enctype="multipart/form-data">');
-        res.write('<input type="file" name="filetoupload"><br>');
-        res.write('<input type="submit">');
-        res.write('</form>');
-        return res.end();
-    }
+    });
 }).listen(config.get('server.port'));
