572f286ca2c16688a2d32953afa5a5f03ecea195
Ravi Bolla <ravi.bolla@everest.engineering>
2022-03-31 14:47:55 +0530

    Adding headers to handle CORS issue

--- node-file-upload/server.js
+++ node-file-upload/server.js
@@ -6,12 +6,18 @@ const config = require('config');
 const filePath = config.get('server.upload_file_path');
 
 http.createServer(function (req, res) {
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+        "Access-Control-Max-Age": 2592000, // 30 days
+    };
     const form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, data) {
         var filepath = data.filetoupload.filepath;
         var newpath = `${filePath}/${data.filetoupload.originalFilename}`;
         fs.rename(filepath, newpath, function (err) {
             if (err) throw err;
+            res.writeHead(200, headers);
             res.write('File uploaded!');
             res.end();
         });
