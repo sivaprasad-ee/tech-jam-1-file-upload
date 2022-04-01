84bf2a6bcb360eae263decd19bacc43d9c5d563b
Ravi Bolla <ravi.bolla@everest.engineering>
2022-04-01 06:23:17 +0530

    back to formidable way of upload

--- node-file-upload/server.js
+++ node-file-upload/server.js
@@ -11,34 +11,22 @@ http.createServer(function (req, res) {
         "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
         "Access-Control-Max-Age": 2592000, // 30 days
     };
-    const stream = fs.createWriteStream(`${uploadFilePath}/file`);
-    // With the open - event, data will start being written
-    // from the request to the stream's destination path
-    stream.on('open', () => {
-        console.log('Stream open ...  0.00%');
-        req.pipe(stream);
+
+    const form = new formidable.IncomingForm();
+    form.on('progress', (bytesReceived, bytesExpected) => {
+        const progress = (bytesReceived / bytesExpected * 100).toFixed(2)
+        console.log(`Processing  ...  ${progress}% done`);
     });
 
-    // Drain is fired whenever a data chunk is written.
-    // When that happens, print how much data has been written yet.
-    stream.on('drain', () => {
-        const written = parseInt(stream.bytesWritten);
-        const total = parseInt(req.headers['content-length']);
-        const pWritten = ((written / total) * 100).toFixed(2);
-        console.log(`Processing  ...  ${pWritten}% done`);
+    form.parse(req, function (err, fields, data) {
+        const filepath = data.filetoupload.filepath;
+        const newpath = `${uploadFilePath}/${data.filetoupload.originalFilename}`;
+        fs.rename(filepath, newpath, function (err) {
+            if (err) throw err;
+            res.writeHead(200, headers);
+            http.createServer(function (req, res) {
+                res.end();
+            });
+        });
     });
-
-    // When the stream is finished, print a final message
-    // Also, resolve the location of the file to calling function
-    stream.on('close', () => {
-        console.log('Processing  ...  100%');
-        res.writeHead(200, headers);
-        res.write('File uploaded!');
-        res.end();
-    });
-    // If something goes wrong, reject the primise
-    stream.on('error', err => {
-        console.error(err);
-        reject(err);
-    });
-}).listen(config.get('server.port'));
+}).listen(config.get('server.port'))
