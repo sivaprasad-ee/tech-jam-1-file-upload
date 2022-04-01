507304c80dfe786289f72ecb683d01d548a00204
Ravi Bolla <ravi.bolla@everest.engineering>
2022-04-01 06:11:44 +0530

    Exploring Multi parser

--- node-file-upload/server.js
+++ node-file-upload/server.js
@@ -11,21 +11,23 @@ http.createServer(function (req, res) {
         "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
         "Access-Control-Max-Age": 2592000, // 30 days
     };
-    const form = new formidable.IncomingForm();
-    form.on('progress', (bytesReceived, bytesExpected) => {
-        const progress = (bytesReceived / bytesExpected * 100).toFixed(2)
-        console.log(`Processing  ...  ${progress}% done`);
-    });
+    const multipartParser = new formidable.MultipartParser();
 
-    form.parse(req, function (err, fields, data) {
-        var filepath = data.filetoupload.filepath;
-        var newpath = `${uploadFilePath}/${data.filetoupload.originalFilename}`;
-        fs.rename(filepath, newpath, function (err) {
-            if (err) throw err;
+    multipartParser.on('data', ({ name, buffer, start, end }) => {
+        console.log(`${name}:`);
+        if (name === 'end') {
             res.writeHead(200, headers);
             res.write('File uploaded!');
             res.end();
-        });
+        }
+        if (buffer && start && end) {
+            console.log(String(buffer.slice(start, end)));
+        }
     });
+    multipartParser.on('error', console.error);
+
+    const boundary = req.headers['content-type'].split('boundary=')[1];
+    multipartParser.initWithBoundary(boundary);
+    req.pipe(multipartParser);
 
 }).listen(config.get('server.port'));
