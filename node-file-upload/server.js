54c570536d4f3fe920b29370e5a1c8c900c60ee7
Ravi Bolla <ravi.bolla@everest.engineering>
2022-04-01 05:58:33 +0530

    Streaming formidable way

--- node-file-upload/server.js
+++ node-file-upload/server.js
@@ -3,7 +3,7 @@ const formidable = require('formidable');
 const fs = require('fs');
 const config = require('config');
 
-const filePath = config.get('server.upload_file_path');
+const uploadFilePath = config.get('server.upload_file_path');
 
 http.createServer(function (req, res) {
     const headers = {
@@ -12,9 +12,14 @@ http.createServer(function (req, res) {
         "Access-Control-Max-Age": 2592000, // 30 days
     };
     const form = new formidable.IncomingForm();
+    form.on('progress', (bytesReceived, bytesExpected) => {
+        const progress = (bytesReceived / bytesExpected * 100).toFixed(2)
+        console.log(`Processing  ...  ${progress}% done`);
+    });
+
     form.parse(req, function (err, fields, data) {
         var filepath = data.filetoupload.filepath;
-        var newpath = `${filePath}/${data.filetoupload.originalFilename}`;
+        var newpath = `${uploadFilePath}/${data.filetoupload.originalFilename}`;
         fs.rename(filepath, newpath, function (err) {
             if (err) throw err;
             res.writeHead(200, headers);
@@ -22,4 +27,5 @@ http.createServer(function (req, res) {
             res.end();
         });
     });
+
 }).listen(config.get('server.port'));
